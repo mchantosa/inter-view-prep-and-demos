ed3459d6e53dcbef475e5e23c245a9f4d039bd0e
Megan Chantosa <meganemmamoore@gmail.com>
2023-04-06 20:22:40 -0700

    converting the wordle command line to a wordle html app, initial commit

--- reductuve/cracklePop.js
+++ reductuve/cracklePop.js
@@ -1,43 +1,43 @@
 /** GIVEN REQUIREMENTS
- * Write a program that prints out the numbers 1 to 100 (inclusive). 
- * If the number is divisible by 3, print Crackle instead of the number. 
- * If it's divisible by 5, print Pop. 
- * If it's divisible by both 3 and 5, print CracklePop. 
- * You can use any language. 
+ * Write a program that prints out the numbers 1 to 100 (inclusive).
+ * If the number is divisible by 3, print Crackle instead of the number.
+ * If it's divisible by 5, print Pop.
+ * If it's divisible by both 3 and 5, print CracklePop.
+ * You can use any language.
 */
 
 /** INTERPRETED REQUIREMENTS
- * Write a program that prints out the numbers 1 to 100 (inclusive). 
- * If the number is divisible by 3, print Crackle instead of the number. 
- * If it's divisible by 5, print Pop [instead of the number]. 
- * If it's divisible by both 3 and 5, print CracklePop [instead of Crackle or Pop]. 
- * You can use any language. 
+ * Write a program that prints out the numbers 1 to 100 (inclusive).
+ * If the number is divisible by 3, print Crackle instead of the number.
+ * If it's divisible by 5, print Pop [instead of the number].
+ * If it's divisible by both 3 and 5, print CracklePop [instead of Crackle or Pop].
+ * You can use any language.
 */
 
 (function () {
-  for(let i = 1; i <= 100; i++){
+  for (let i = 1; i <= 100; i++) {
     let log = '';
-    if(i%3 === 0)log += 'Crackle';
-    if(i%5 === 0)log += 'Pop'
+    if (i % 3 === 0)log += 'Crackle';
+    if (i % 5 === 0)log += 'Pop';
     log = log || i;
     console.log(log);
   }
-})();
+}());
 
 /** LITERAL REQUIREMENTS (Just incase)
  * Assumptions made: Went with a literal interpretation of requirements
- * - Print "Crackle" instead of number 
- *    - But not 'Pop' INSTEAD of number 
+ * - Print "Crackle" instead of number
+ *    - But not 'Pop' INSTEAD of number
  *    - But not 'CracklePop' INSTEAD of number
  *    - And not 'CracklePop' INSTEAD of 'Crackle' or 'Pop'
  */
 
-(function(){
-  for(let i = 1; i <= 100; i++){
-    if(i%3 === 0){
+(function () {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
       console.log('Crackle');
-    } else console.log(i)
-    if(i%5 === 0) console.log('Pop')
-    if((i%5 === 0)&&(i%3===0)) console.log('CracklePop')
+    } else console.log(i);
+    if (i % 5 === 0) console.log('Pop');
+    if ((i % 5 === 0) && (i % 3 === 0)) console.log('CracklePop');
   }
-})();
+}());
